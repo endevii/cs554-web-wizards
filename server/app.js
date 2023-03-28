4853ca38fb433ba095dd6c394303b779d2a19041
Joshua Gorman <90810000+endevii@users.noreply.github.com>
2023-03-28 19:16:02 -0400

    Added initial mongo and app.js in server folder

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const configRoutes = require("./routes");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(
+  session({
+    name: "AuthCookie",
+    secret: "some secret string!",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+configRoutes(app);
+
+app.listen(3001, function () {
+  logger.info("Server Started on http://localhost:3001");
+});
