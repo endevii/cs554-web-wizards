996c321972edaa4acba99856ffdf2dc551ac0fdc
Jasmine Perez <jasperez31@gmail.com>
2023-05-07 18:40:26 -0400

    add review cards

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,27 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import SignOutButton from './SignOut';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import SignOutButton from "./SignOut";
+import axios from "axios";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 function Account() {
-  //const [uid, setUid] = useState(null);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [loadingUser, setLoadingUser] = useState(true);
   const [mongoUser, setMongoUser] = useState(null);
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const navigate = useNavigate();
   let card = null;
-  //const [file, setFile] = useState(null);
-  //const [upload, setUpload] = useState("https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436191.jpg?size=626&ext=jpg&ga=GA1.1.55329923.1683053289&semt=robertav1_2_sidr");
   let auth = getAuth();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
-        //le.log(user);
         setName(user.displayName);
         setLoading(false);
       } else {
@@ -32,7 +36,7 @@ function Account() {
 
   useEffect(() => {
     const getUser = async (uid) => {
-      const { data } = await axios.get('http://localhost:3001/user/' + uid);
+      const { data } = await axios.get("http://localhost:3001/user/" + uid);
       setMongoUser(data);
       setLoadingUser(false);
     };
@@ -43,15 +47,15 @@ function Account() {
 
   const buildSiteCard = (site) => {
     return (
-      <div key={'div' + site._id} className='revolution'>
+      <div key={"div" + site._id} className="revolution">
         <hr />
-        <h1 className='itinerary-stop-title'>{site.name}</h1>
-        <div className='itinerary-container'>
-          <div className='customImageItinerary'>
-            <img className='resize-image' src={site.image} alt='federal hall' />
+        <h1 className="itinerary-stop-title">{site.name}</h1>
+        <div className="itinerary-container">
+          <div className="customImageItinerary">
+            <img className="resize-image" src={site.image} alt={site.name} />
           </div>
-          <div className='grid-item step-description'>
-            <ul id='description-list'>
+          <div className="grid-item step-description">
+            <ul id="description-list">
               {site.description.map((line) => (
                 <li key={line}>{line}</li>
               ))}
@@ -68,36 +72,35 @@ function Account() {
     mongoUser.itineraries.map((itinerary) => {
       return (
         <div key={itinerary.ids}>
-          <h2>Itinerary</h2>
+          <h2 className="itinerary-title">Itinerary</h2>
           {itinerary.itinerary.map((site) => {
             return <div key={site._id}>{buildSiteCard(site)}</div>;
           })}
           {!loading && !loadingUser && (
             <button
               onClick={async (e) => {
-                //console.log(sites)
                 try {
                   const { data } = await axios.get(
-                    'http://localhost:3001/deleteItinerary/' + user.uid,
+                    "http://localhost:3001/deleteItinerary/" + user.uid,
                     {
                       params: {
                         itinerary: JSON.stringify(itinerary),
                       },
                     }
                   );
-                  //console.log(data);
                   setMongoUser(data);
-                  alert('Itinerary unsaved');
+                  alert("Itinerary unsaved");
                   window.location.reload();
                 } catch (e) {
-                  alert('Error: You have not saved this itinerary');
+                  alert("Error: You have not saved this itinerary");
                 }
-              }}>
+              }}
+            >
               Unsave Itinerary
             </button>
           )}
-          <hr className='hr-custom' />
-          <hr className='hr-custom' />
+          <hr className="hr-custom" />
+          <hr className="hr-custom" />
           <br />
         </div>
       );
@@ -105,24 +108,23 @@ function Account() {
 
   if (!loading && user.displayName && !loadingUser) {
     return (
-      <div className='account-wrapper'>
-        <h1 className='grid-item account-title'>
+      <div className="account-wrapper">
+        <h1 className="grid-item account-title">
           {user.displayName}'s Account
         </h1>
-        <div className='account-signout'>
+        <div className="account-signout">
           <SignOutButton />
-          <form action='/changepassword'>
-            <button type='submit'>Change Password</button>
+          <form action="/changepassword">
+            <button type="submit">Change Password</button>
           </form>
         </div>
         <br />
-        <hr className='hr-custom' />
         <div>
           {mongoUser &&
           mongoUser.itineraries &&
           mongoUser.itineraries.length !== 0 ? (
-            <div>
-              <h2>Your saved itineraries:</h2>
+            <div className="itineraries">
+              <h2>Your saved itineraries</h2>
               <div>{card}</div>
             </div>
           ) : (
@@ -130,51 +132,73 @@ function Account() {
           )}
         </div>
         <br />
-        <hr className='hr-custom' />
-        <hr className='hr-custom' />
         <div>
           {mongoUser && mongoUser.reviews && mongoUser.reviews.length !== 0 ? (
             <div>
-              <h2 className='title-account'>Your Reviews</h2>
-              <ul>
+              <h2 className="title-account">Your Reviews</h2>
+              <Grid
+                container
+                spacing={2}
+                sx={{ flexGrow: 1, flexDirection: "row", justifyContent: "center" }}
+              >
                 {mongoUser.reviews.map((review) => {
                   return (
-                    <div key={review._id} className='div-class'>
-                      <p>Site: {review.siteName}</p>
-                      <p>Title: {review.title}</p>
-                      <p>Review: {review.review}</p>
-                      <p>Rating: {review.rating}</p>
-                      <p>Date: {review.date}</p>
-                      <button
-                        onClick={async (e) => {
-                          try {
-                            await axios.delete(
-                              'http://localhost:3001/reviews/' +
-                                review.siteid +
-                                '/' +
-                                mongoUser.uid +
-                                '/' +
-                                review._id
-                            );
-                          } catch (e) {
-                            alert('Error: Unable to delete review');
-                          }
-                          window.location.reload();
-                        }}>
-                        Delete Review
-                      </button>
-                    </div>
+                    <Grid
+                      item
+                      xs={12}
+                      sm={7}
+                      md={5}
+                      lg={4}
+                      xl={3}
+                      key={review._id}
+                    >
+                      <Card>
+                        <CardActionArea>
+                          <Link to={`/site/${review.siteid}`}>
+                            <CardContent>
+                              <Typography className="review-txt">
+                                {" "}
+                                Site: {review.siteName} <br/>
+                                Title: {review.title} <br/>
+                                Review: {review.review} <br/>
+                                Rating: {review.rating} <br/>
+                                Date: {review.date}
+                              </Typography>
+                            </CardContent>
+                          </Link>
+                        </CardActionArea>
+                        <CardContent>
+                          <button
+                            onClick={async (e) => {
+                              try {
+                                await axios.delete(
+                                  "http://localhost:3001/reviews/" +
+                                    review.siteid +
+                                    "/" +
+                                    mongoUser.uid +
+                                    "/" +
+                                    review._id
+                                );
+                              } catch (e) {
+                                alert("Error: Unable to delete review");
+                              }
+                              window.location.reload();
+                            }}
+                          >
+                            Delete Review
+                          </button>
+                        </CardContent>
+                      </Card>
+                    </Grid>
                   );
                 })}
-              </ul>
+              </Grid>
             </div>
           ) : (
-            <h2 className='title-account'>You do not have any reviews</h2>
+            <h2 className="title-account">You do not have any reviews</h2>
           )}
         </div>
         <br />
-        <hr className='hr-custom' />
-        <hr className='hr-custom' />
       </div>
     );
   } else if (loading) {
