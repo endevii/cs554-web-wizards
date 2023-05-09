8e8da02eec76c7c7e0fb22c27ca6568cf564ffc2
Joshua Gorman <90810000+endevii@users.noreply.github.com>
2023-05-09 18:50:09 -0400

    Hopefully fixed remaining tota11y issues

--- client/src/components/IndividualSite.js
+++ client/src/components/IndividualSite.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import axios from 'axios';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import Chat from './Chat';
-import PostReview from './PostReview';
-import Error from './Error';
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Chat from "./Chat";
+import PostReview from "./PostReview";
+import Error from "./Error";
 
 function IndividualSite() {
   let { id } = useParams();
@@ -16,13 +16,13 @@ function IndividualSite() {
 
   useEffect(() => {
     const getSite = async () => {
-      try{
+      try {
         const { data } = await axios.get(
-        `http://localhost:3001/site/${currentSiteId}`
+          `http://localhost:3001/site/${currentSiteId}`
         );
         setSiteData(data);
         setLoading(false);
-      } catch(e) {
+      } catch (e) {
         setError(e);
       }
     };
@@ -46,88 +46,113 @@ function IndividualSite() {
 
   if (!loading && !uidLoading && siteData) {
     return (
-      <div className='wrapper'>
-        <h1 className='grid-item title'>
+      <div className="wrapper">
+        <h1 className="grid-item title">
           {siteData.name}
           <br />
-          <Link to='/sites' className='button'>
+          <Link to="/sites" className="button">
             All Sites
           </Link>
         </h1>
-        <div className='grid-item image'>
-          <img src={`/img/${siteData.name.replaceAll(" ", "").replaceAll("/", "")}.jpeg`} className='card-img-top' alt={siteData.name} />
+        <div className="grid-item image">
+          <img
+            src={`/img/${siteData.name
+              .replaceAll(" ", "")
+              .replaceAll("/", "")}.jpeg`}
+            className="card-img-top"
+            alt={siteData.name}
+          />
         </div>
-        <div className='grid-item address'>
+        <div className="grid-item address">
           <h2>Location:</h2>
           <ul>
             <li>Borough: {siteData.borough}</li>
             <li>
-              Address: {siteData.location.address}, {siteData.location.city},{' '}
+              Address: {siteData.location.address}, {siteData.location.city},{" "}
               {siteData.location.state}, {siteData.location.zipCode}
             </li>
           </ul>
         </div>
-        <div className='grid-item facts'>
+        <div className="grid-item facts">
           <h2>Quick Facts</h2>
           <ul>
             <li>
-              {siteData.name} is open {siteData.hours.days} from{' '}
+              {siteData.name} is open {siteData.hours.days} from{" "}
               {siteData.hours.time}
             </li>
             {!siteData.reviews ? (
               <li>This location has yet to be reviewed by anyone</li>
             ) : (
               <li>
-                It has a rating of {siteData.rating} out of{' '}
+                It has a rating of {siteData.rating} out of{" "}
                 {siteData.reviews.length} reviews
               </li>
             )}
             <li>
-              Find more information <Link to={siteData.website} target="_blank">here</Link>
+              Find more information{" "}
+              <Link
+                to={siteData.website}
+                aria-label={siteData.name}
+                className="web-link"
+                target="_blank"
+              >
+                here
+              </Link>
             </li>
           </ul>
         </div>
-        <div className='grid-item description'>
+        <div className="grid-item description">
           <h2>Description</h2>
-          <ul id='description-list'>
+          <ul id="description-list">
             {siteData.description.map((line) => (
               <li key={line}>{line}</li>
             ))}
           </ul>
         </div>
         {uid ? (
-          <div className='grid-item reviews'>
+          <div className="grid-item reviews">
             <h2>Reviews</h2>
-            <ul id='reviewList'>
+            <ul id="reviewList">
               {siteData.reviews.map((review) => (
                 <li key={review._id}>
-                  <div className='review-li'>
-                    <p className='review-user'>{review.userName}</p>
-                    <p>Rating: {review.rating}/5 {" "}<span>{review.title}</span></p>
-                    <p>Reviewed on {review.date} {review.edited? "(edited)": ""}</p>
-                    <p>{review.review}</p><br/>
+                  <div className="review-li">
+                    <p className="review-user">{review.userName}</p>
+                    <p>
+                      Rating: {review.rating}/5 <span>{review.title}</span>
+                    </p>
+                    <p>
+                      Reviewed on {review.date}{" "}
+                      {review.edited ? "(edited)" : ""}
+                    </p>
+                    <p>{review.review}</p>
+                    <br />
                   </div>
                 </li>
               ))}
             </ul>
-            <button className="review-tog" onClick={() => setButtonToggle(!buttonToggle)}>Post your own review {buttonToggle? "-": "+"}</button>
+            <button
+              className="review-tog"
+              onClick={() => setButtonToggle(!buttonToggle)}
+            >
+              Post your own review {buttonToggle ? "-" : "+"}
+            </button>
             {buttonToggle && <PostReview site={siteData} />}
           </div>
         ) : (
-          <div className='grid-item reviews'>Login to see reviews</div>
+          <div className="grid-item reviews">Login to see reviews</div>
         )}
         {uid ? (
-          <div className=' grid-item chat'>
+          <div className=" grid-item chat">
             <Chat thisRoom={siteData.name}></Chat>
           </div>
         ) : (
-          <div className='grid-item chat'>Login to join the chat</div>
+          <div className="grid-item chat">Login to join the chat</div>
         )}
       </div>
     );
   } else if (error) {
     return <Error />;
-  } else if(loading){
+  } else if (loading) {
     return <div>Loading...</div>;
   }
 }
--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useState, useEffect } from "react";
 
 function NavigationBar() {
   const [userExists, setUserExists] = useState(false);
@@ -21,36 +21,36 @@ function NavigationBar() {
 
   return (
     <>
-      <nav className='navbar navbar-expand-lg navbar-light' id='navbar-custom'>
-        <a className='navbar-brand' href='/'>
+      <nav className="navbar navbar-expand-lg navbar-light" id="navbar-custom">
+        <a className="navbar-brand" style={{ color: "white" }} href="/">
           NYC Historical Sites
         </a>
 
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav mr-auto'>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/' id='link-txt'>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/" id="link-txt1">
                 Home
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/sites' id='link-txt'>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sites" id="link-txt2">
                 Sites
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/map' id='link-txt'>
+            <li className="nav-item">
+              <Link className="nav-link" to="/map" id="link-txt3">
                 Map
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/itineraries' id='link-txt'>
+            <li className="nav-item">
+              <Link className="nav-link" to="/itineraries" id="link-txt4">
                 Itineraries
               </Link>
             </li>
             {!loading && userExists && (
-              <li className='nav-item'>
-                <Link className='nav-link' to='/requestSite' id='link-txt'>
+              <li className="nav-item">
+                <Link className="nav-link" to="/requestSite" id="link-txt5">
                   Request Site
                 </Link>
               </li>
@@ -60,22 +60,16 @@ function NavigationBar() {
             <div>
               {!userExists ? (
                 <div>
-                  <Link to='/signin'>
-                    <button
-                      className='btn btn-outline-light my-2 my-sm-0'
-                      type='submit'
-                      style={{ backgroundColor: '#c34a4a' }}>
+                  <Link to="/signin">
+                    <button className="btn btn-log" type="submit">
                       Login
                     </button>
                   </Link>
                 </div>
               ) : (
                 <div>
-                  <Link to='/account'>
-                    <button
-                      className='btn btn-outline-light my-2 my-sm-0'
-                      type='submit'
-                      style={{ backgroundColor: '#c34a4a' }}>
+                  <Link to="/account">
+                    <button className="btn btn-log" type="submit">
                       Account
                     </button>
                   </Link>
